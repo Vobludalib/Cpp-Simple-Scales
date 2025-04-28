c3826b5789fad27c549ffa062b75f6b48f3b7f96
librickysimon <librickysimon@gmail.com>
2025-04-28 12:08:33 +0200

    Further work on scalemanager

--- scalemanager.cpp
+++ scalemanager.cpp
@@ -1,7 +1,9 @@
 #include "scalemanager.hpp"
 
 #include <algorithm>
+#include <format>
 #include <random>
+#include <set>
 
 void ScaleManager::handle_file(std::string path)
 {
@@ -66,7 +68,7 @@ void ScaleManager::parse_fstream(std::ifstream& stream)
                     }
                     else
                     {
-                        throw std::runtime_error(INVALID_DIFFICULTY);
+                        throw std::runtime_error(std::format(INVALID_DIFFICULTY, row, column));
                     }
                     break;
                 }
@@ -100,14 +102,15 @@ void ScaleManager::load_scales_from_file(std::string path)
     build_maps();
 }
 
-std::vector<ScaleManager::ScaleEntry*> ScaleManager::get_random_scales(size_t number_of_scales)
+std::vector<ScaleManager::ScaleEntry<Scale>*> ScaleManager::get_random_scales(
+    size_t number_of_scales)
 {
     if (number_of_scales > _entries.size())
     {
         throw std::invalid_argument(TOO_MANY_SAMPLES);
     }
 
-    std::vector<ScaleEntry*> pointers;
+    std::vector<ScaleEntry<Scale>*> pointers;
     pointers.reserve(_entries.size());
 
     // Fill with pointers
@@ -116,14 +119,101 @@ std::vector<ScaleManager::ScaleEntry*> ScaleManager::get_random_scales(size_t nu
         pointers.push_back(&entry);
     }
 
-    std::vector<ScaleEntry*> result;
+    std::vector<ScaleEntry<Scale>*> result;
     result.reserve(number_of_scales);
 
     std::random_device rd;
     std::mt19937 gen(rd());
 
     // Sample n pointers into result
-    std::ranges::sample(pointers, std::back_inserter(result), number_of_scales, gen);
+    std::ranges::sample(pointers, std::back_inserter(result), static_cast<long>(number_of_scales),
+                        gen);
 
     return result;
+}
+
+std::vector<ScaleManager::ScaleEntry<Scale>*> ScaleManager::get_random_scales_by_difficulty(
+    size_t number_of_scales, ScaleManager::Difficulty difficulty)
+{
+    std::vector<ScaleManager::ScaleEntry<Scale>*> sampled_scales;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::discrete_distribution<size_t> diff_dist(static_cast<size_t>(difficulty) + 1, 0,
+                                                 static_cast<size_t>(difficulty),
+                                                 [](double) { return 1.0; });
+
+    std::vector<size_t> diff_counts{_difficulty_map.count(ScaleManager::Difficulty::EASY),
+                                    _difficulty_map.count(ScaleManager::Difficulty::MEDIUM),
+                                    _difficulty_map.count(ScaleManager::Difficulty::HARD)};
+
+    std::vector<size_t> sampled_difficulties;
+    for (size_t i = 0; i < number_of_scales; ++i)
+    {
+        // Logic to prevent sampling a difficulty without any scales present
+        size_t sampled_difficulty;
+        bool sampled = false;
+        while (!sampled || diff_counts[sampled_difficulty] == 0)
+        {
+            sampled_difficulty = diff_dist(gen);
+            sampled = true;
+        }
+        sampled_difficulties.emplace_back(sampled_difficulty);
+    }
+
+    std::vector<std::discrete_distribution<size_t>> individual_difficulty_distributions;
+    for (auto&& diff_count : diff_counts)
+    {
+        individual_difficulty_distributions.emplace_back(
+            diff_count, (double)0, (double)(diff_count - 1), [](double) { return 1.0; });
+    }
+
+    for (auto&& d : sampled_difficulties)
+    {
+        auto scales_for_d = _difficulty_map.equal_range(static_cast<ScaleManager::Difficulty>(d));
+        auto chosen_scale_in_diff = individual_difficulty_distributions[d](gen);
+        std::advance(scales_for_d.first, chosen_scale_in_diff);
+        sampled_scales.push_back(scales_for_d.first->second);
+    }
+
+    return sampled_scales;
+}
+
+std::vector<Note*> ScaleManager::get_random_roots_by_difficulty(size_t number_of_roots,
+                                                                ScaleManager::Difficulty difficulty)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::discrete_distribution<size_t> root_note_dist(
+        _possible_roots.size(), 0, _possible_roots.size() - 1, [difficulty](size_t i)
+        { return _root_note_weights_by_difficulty[static_cast<size_t>(difficulty)][i]; });
+
+    std::vector<Note*> sampled_notes;
+
+    for (size_t i = 0; i < number_of_roots; ++i)
+    {
+        size_t sampled_index = root_note_dist(gen);
+        sampled_notes.push_back(&_possible_roots[sampled_index]);
+    }
+
+    return sampled_notes;
+}
+
+std::vector<ScaleManager::ScaleEntry<RealisedScale>>
+ScaleManager::generate_realised_scales_by_difficulty(size_t number_of_scales,
+                                                     ScaleManager::Difficulty difficulty)
+{
+    auto scales = get_random_scales_by_difficulty(number_of_scales, difficulty);
+    auto roots = get_random_roots_by_difficulty(number_of_scales, difficulty);
+
+    std::vector<ScaleManager::ScaleEntry<RealisedScale>> output;
+    for (size_t i = 0; i < number_of_scales; ++i)
+    {
+        output.emplace_back(RealisedScale{*roots[i], scales[i]->get_scale()},
+                            scales[i]->get_difficulty(), scales[i]->get_name());
+    }
+
+    return output;
 }
--- scalemanager.hpp
+++ scalemanager.hpp
@@ -8,13 +8,14 @@
 #include "musiclibrary.hpp"
 
 constexpr char BAD_FILE_OPEN[] = "Unable to open the file!";
-constexpr char INVALID_DIFFICULTY[] = "Invalid difficulty value found during parsing file!";
+constexpr char INVALID_DIFFICULTY[] =
+    "Invalid difficulty value found during parsing file! Row: {}, Column: {}";
 constexpr char TOO_MANY_SAMPLES[] = "Too many samples requested!";
 constexpr char CSV_SEPERATOR = ';';
 
 class ScaleManager
 {
-   private:
+   public:
     enum class Difficulty
     {
         EASY = 0,
@@ -22,42 +23,63 @@ class ScaleManager
         HARD
     };
 
+   private:
+    template <typename T>
     struct ScaleEntry
     {
        private:
-        Scale _scale;
+        T _scale;
         ScaleManager::Difficulty _difficulty;
         std::string _name;
 
        public:
-        inline ScaleEntry(const Scale& scale, const ScaleManager::Difficulty& difficulty,
+        inline ScaleEntry(const T& scale, const ScaleManager::Difficulty& difficulty,
                           const std::string& name)
             : _scale(scale), _difficulty(difficulty), _name(name)
         {
         }
 
-        inline ScaleEntry(Scale&& scale, ScaleManager::Difficulty&& difficulty, std::string&& name)
+        inline ScaleEntry(T&& scale, ScaleManager::Difficulty&& difficulty, std::string&& name)
             : _scale(std::move(scale)), _difficulty(std::move(difficulty)), _name(std::move(name))
         {
         }
 
-        inline const Scale& get_scale() const { return _scale; }
+        inline const T& get_scale() const { return _scale; }
         inline const ScaleManager::Difficulty& get_difficulty() const { return _difficulty; }
         inline const std::string& get_name() const { return _name; }
     };
 
     // Vector actually owns all the entries, the other containers just reference elements of the
     // vector
-    std::vector<ScaleEntry> _entries;
-    std::multimap<Difficulty, ScaleEntry*> _difficulty_map;
+    std::vector<ScaleEntry<Scale>> _entries;
+    std::multimap<Difficulty, ScaleEntry<Scale>*> _difficulty_map;
+
+    inline static Note _middle_c{};
+    inline static std::vector<Note> _possible_roots{
+        {_middle_c, 1, 0}, {_middle_c, 2, -1}, {_middle_c, 2, 0}, {_middle_c, 3, -1},
+        {_middle_c, 3, 0}, {_middle_c, 4, 0},  {_middle_c, 4, 1}, {_middle_c, 5, -1},
+        {_middle_c, 5, 0}, {_middle_c, 6, -1}, {_middle_c, 6, 0}, {_middle_c, 7, -1},
+        {_middle_c, 7, 0}};
+
+    inline static std::vector<std::vector<double>> _root_note_weights_by_difficulty{
+        {1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0},
+        {1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0},
+        {2, 1, 2, 2, 2, 2, 1, 1, 2, 1, 2, 2, 1}};
 
     void handle_file(std::string path);
     void parse_fstream(std::ifstream& stream);
     void build_maps();
 
+    std::vector<ScaleEntry<Scale>*> get_random_scales(size_t number_of_scales);
+    std::vector<ScaleEntry<Scale>*> get_random_scales_by_difficulty(
+        size_t number_of_scales, ScaleManager::Difficulty difficulty);
+    std::vector<Note*> get_random_roots_by_difficulty(size_t number_of_roots,
+                                                      ScaleManager::Difficulty difficulty);
+
    public:
     void load_scales_from_file(std::string path);
-    std::vector<ScaleEntry*> get_random_scales(size_t number_of_scales);
+    std::vector<ScaleManager::ScaleEntry<RealisedScale>> generate_realised_scales_by_difficulty(
+        size_t number_of_scales, ScaleManager::Difficulty difficulty);
 };
 
 #endif
