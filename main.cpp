05057734c1df50b20ca7bb26d15e2adbcc3d0916
librickysimon <librickysimon@gmail.com>
2025-04-29 15:55:24 +0200

    Functional main mockup

--- main.cpp
+++ main.cpp
@@ -3,13 +3,23 @@
 
 // #define GERMAN_NAMING
 
+#include "applicationmanager.hpp"
 #include "musiclibrary.hpp"
 #include "scalemanager.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
-    ScaleManager sm;
-    sm.load_scales_from_file("./scales.csv");
-    auto&& scale_entry = sm.get_random_scales(1);
-    std::cout << scale_entry[0]->get_name() << std::endl;
+    ApplicationManager am;
+    am.load_scales("./scales.csv");
+    am.generate_session((size_t)2, ScaleManager::Difficulty::HARD);
+    while (am.can_print_more())
+    {
+        am.print_header(std::cout);
+        am.print_question(std::cout);
+        am.load_answer(std::cin);
+        am.next_question();
+        am.clear_stream(std::cout);
+    }
+
+    std::cout << "You got " << am.get_success_percentage() << "% correct!" << std::endl;
 }
